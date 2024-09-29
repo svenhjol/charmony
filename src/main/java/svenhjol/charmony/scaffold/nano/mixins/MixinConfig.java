2671e3277006823936a2379e5a9aa2571d7cacfd
Sven H <sven.hjol@protonmail.com>
2024-09-29 17:42:00 +0000

    Mixin configuration and feature checking

--- src/main/java/svenhjol/charmony/scaffold/nano/mixins/MixinConfig.java
+++ src/main/java/svenhjol/charmony/scaffold/nano/mixins/MixinConfig.java
@@ -1,4 +1,15 @@
 package svenhjol.charmony.scaffold.nano.mixins;
 
+import svenhjol.charmony.scaffold.nano.Charmony;
+
 public class MixinConfig extends BaseMixinConfig {
+    @Override
+    protected String modId() {
+        return Charmony.ID;
+    }
+
+    @Override
+    protected String rootClassPath() {
+        return "svenhjol.charmony.scaffold.nano";
+    }
 }
