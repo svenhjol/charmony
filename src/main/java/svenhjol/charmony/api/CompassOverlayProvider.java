36c836cfeca56491b9c444e0c63aec6afae2c42e
Sven H <sven.hjol@protonmail.com>
2024-09-30 01:15:12 +0000

    Strip out test things, move to the tweaks mod

--- src/main/java/svenhjol/charmony/api/CompassOverlayProvider.java
+++
@@ -1,7 +0,0 @@
-package svenhjol.charmony.api;
-
-import java.util.List;
-
-public interface CompassOverlayProvider {
-    List<CompassOverlayItem> getCompassOverlayItems();
-}
--- src/main/java/svenhjol/charmony/scaffold/nano/fabric/ClientInitializer.java
+++ src/main/java/svenhjol/charmony/scaffold/nano/fabric/ClientInitializer.java
@@ -1,26 +1,11 @@
 package svenhjol.charmony.scaffold.nano.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
-import svenhjol.charmony.scaffold.nano.Charmony;
 import svenhjol.charmony.scaffold.nano.client.ClientEvents;
-import svenhjol.charmony.scaffold.nano.client.compasses_show_position.CompassesShowPosition;
-import svenhjol.charmony.scaffold.nano.client.repair_cost_visible.RepairCostVisible;
-import svenhjol.charmony.scaffold.nano.enums.Side;
 
 public class ClientInitializer implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientEvents.init();
-
-        // Test data follows.
-        var charmony = Charmony.instance();
-        var feature = new RepairCostVisible(charmony);
-
-        // Add features.
-        charmony.addFeature(new CompassesShowPosition(charmony));
-        charmony.addFeature(new RepairCostVisible(charmony));
-
-        // Run the mod.
-        charmony.run(Side.Client);
     }
 }
