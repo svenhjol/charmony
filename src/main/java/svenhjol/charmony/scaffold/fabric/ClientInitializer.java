4dbe593335a665b2393bd27d32a3a4f9b10ed2f9
Sven H <sven.hjol@protonmail.com>
2024-10-03 11:00:09 +0000

    Expose init() method for child mods to ensure that Charmony gets launched first

--- src/main/java/svenhjol/charmony/scaffold/fabric/ClientInitializer.java
+++ src/main/java/svenhjol/charmony/scaffold/fabric/ClientInitializer.java
@@ -7,8 +7,18 @@ import svenhjol.charmony.scaffold.client.diagnostics.Diagnostics;
 import svenhjol.charmony.scaffold.enums.Side;
 
 public class ClientInitializer implements ClientModInitializer {
+    private static boolean initialized = false;
+
     @Override
     public void onInitializeClient() {
+        init();
+    }
+
+    /**
+     * We expose init() so that child mods can ensure that Charmony gets launched first.
+     */
+    public static void init() {
+        if (initialized) return;
         ClientEvents.init();
 
         var charmony = Charmony.instance();
@@ -17,5 +27,6 @@ public class ClientInitializer implements ClientModInitializer {
         charmony.addFeature(Diagnostics.class);
 
         charmony.run(Side.Client);
+        initialized = true;
     }
 }
