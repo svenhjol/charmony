9d86dd48ebf794f3e196ad134c8b65ed7194cbc9
Sven H <sven.hjol@protonmail.com>
2024-12-06 17:20:28 +0000

    First pass ItemDragDrop callback

---
+++ src/main/java/svenhjol/charmony/core/common/mixins/item_drag_drop_callback/ItemMixin.java
@@ -0,0 +1,62 @@
+package svenhjol.charmony.core.common.mixins.item_drag_drop_callback;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.SlotAccess;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import svenhjol.charmony.core.events.ItemDragDropCallback;
+
+import javax.annotation.Nullable;
+
+@Mixin(Item.class)
+public class ItemMixin {
+
+    @ModifyReturnValue(
+            method = "overrideStackedOnOther",
+            at = @At("RETURN")
+    )
+    private boolean hookOverrideStackedOnOther(boolean original,
+                                               @Local(argsOnly = true) ItemStack source,
+                                               @Local(argsOnly = true) Slot slot,
+                                               @Local(argsOnly = true) ClickAction clickAction,
+                                               @Local(argsOnly = true) Player player
+    ) {
+        var result = performStack(ItemDragDropCallback.StackType.STACKED_ON_OTHER, source, slot.getItem(), slot, clickAction, player, null);
+        if (result != InteractionResult.PASS) {
+            return true;
+        }
+        return original;
+    }
+
+    @ModifyReturnValue(
+            method = "overrideOtherStackedOnMe",
+            at = @At("RETURN")
+    )
+    private boolean hookOverrideOtherStackedOnMe(boolean original,
+                                                 @Local(ordinal = 0, argsOnly = true) ItemStack source,
+                                                 @Local(ordinal = 1, argsOnly = true) ItemStack dest,
+                                                 @Local(argsOnly = true) Slot slot,
+                                                 @Local(argsOnly = true) ClickAction clickAction,
+                                                 @Local(argsOnly = true) Player player,
+                                                 @Local(argsOnly = true) SlotAccess slotAccess
+    ) {
+        var result = performStack(ItemDragDropCallback.StackType.STACKED_ON_SELF, source, dest, slot, clickAction, player, slotAccess);
+        if (result != InteractionResult.PASS) {
+            return true;
+        }
+        return original;
+    }
+
+    @Unique
+    private InteractionResult performStack(ItemDragDropCallback.StackType stackType, ItemStack source, ItemStack dest, Slot slot, ClickAction clickAction, Player player, @Nullable SlotAccess slotAccess) {
+        return ItemDragDropCallback.EVENT.invoker().interact(stackType, source, dest, slot, clickAction, player, slotAccess);
+    }
+}
