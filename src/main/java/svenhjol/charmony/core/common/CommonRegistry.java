e5d244288a1c0eeda87bd3fd3c121e638739cd98
Sven H <sven.hjol@protonmail.com>
2024-12-12 21:43:49 +0000

    Register block entity and data component in CommonRegistry

--- src/main/java/svenhjol/charmony/core/common/CommonRegistry.java
+++ src/main/java/svenhjol/charmony/core/common/CommonRegistry.java
@@ -1,16 +1,24 @@
 package svenhjol.charmony.core.common;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import svenhjol.charmony.core.helper.VillagerHelper;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 import static net.minecraft.world.entity.npc.VillagerTrades.WANDERING_TRADER_TRADES;
 
@@ -23,6 +31,21 @@ public final class CommonRegistry {
             instance = new CommonRegistry();
         }
         return instance;
+    }
+
+    public <T extends BlockEntity, U extends Block> BlockEntityType<T> blockEntity(ResourceLocation id, FabricBlockEntityTypeBuilder.Factory<T> builder, List<U> blocks) {
+        var blocksToAdd = blocks.toArray(Block[]::new);
+
+        return Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, id,
+            FabricBlockEntityTypeBuilder.create(builder, blocksToAdd).build());
+    }
+
+    public <T extends BlockEntity> BlockEntityType<T> blockEntity(ResourceLocation id, FabricBlockEntityTypeBuilder.Factory<T> builder) {
+        return blockEntity(id, builder, List.of());
+    }
+
+    public <T> DataComponentType<T> dataComponent(ResourceLocation id, Supplier<UnaryOperator<DataComponentType.Builder<T>>> dataComponent) {
+        return DataComponents.register(id.toString(), dataComponent.get());
     }
 
     /**
