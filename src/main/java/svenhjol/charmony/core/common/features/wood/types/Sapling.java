b27f3e29d7a4dfdcc446a73af967597704edadee
Sven H <sven.hjol@protonmail.com>
2025-04-06 21:20:27 +0000

    Wrap sapling in register check

--- src/main/java/svenhjol/charmony/core/common/features/wood/types/Sapling.java
+++ src/main/java/svenhjol/charmony/core/common/features/wood/types/Sapling.java
@@ -1,5 +1,7 @@
 package svenhjol.charmony.core.common.features.wood.types;
 
+import net.minecraft.world.level.block.grower.TreeGrower;
+import svenhjol.charmony.core.base.Registerable;
 import svenhjol.charmony.core.common.features.wood.CustomWood;
 import svenhjol.charmony.core.common.features.wood.CustomWoodType;
 import svenhjol.charmony.core.common.features.wood.WoodMaterial;
@@ -12,16 +14,22 @@ public class Sapling extends CustomWoodType {
     public final Supplier<CustomSaplingBlock> block;
     public final Supplier<CustomSaplingBlock.SaplingBlockItem> item;
 
+    public Supplier<TreeGrower> tree;
+
     public Sapling(WoodRegistry woodRegistry, WoodMaterial material) {
         super(woodRegistry, material);
 
         var commonRegistry = woodRegistry.commonRegistry();
         var saplingId = material.getSerializedName() + "_sapling";
 
-        var tree = material.tree();
-        if (tree.isEmpty()) {
-            throw new RuntimeException("No tree defined for sapling!");
-        }
+        new Registerable<>(feature(), () -> {
+            var tree = material.tree();
+            if (tree.isEmpty()) {
+                throw new RuntimeException("No tree defined for sapling!");
+            }
+            this.tree = tree::get;
+            return null;
+        });
 
         block = commonRegistry.block(saplingId, key -> new CustomSaplingBlock(key, material, tree.get()));
         item = commonRegistry.item(saplingId, key -> new CustomSaplingBlock.SaplingBlockItem(key, block));
