4c681877afd6910456416a62725fcd1c23d930c8
Sven H <sven.hjol@protonmail.com>
2025-06-02 23:06:13 +0100

    Prep modmenu support

--- src/main/java/svenhjol/charmony/core/integration/ModMenuPlugin.java
+++ src/main/java/svenhjol/charmony/core/integration/ModMenuPlugin.java
@@ -1,12 +1,13 @@
 package svenhjol.charmony.core.integration;
 
-//import svenhjol.charmony.core.Charmony;
+//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+//import com.terraformersmc.modmenu.api.ModMenuApi;
 //import svenhjol.charmony.core.base.Mod;
+//import svenhjol.charmony.core.client.features.control_panel.ModsScreen;
 //
-//@SuppressWarnings("unused")
-//public final class ModMenuPlugin extends BaseModMenuPlugin {
+//public class ModMenuPlugin implements ModMenuApi {
 //    @Override
-//    public Mod mod() {
-//        return Charmony.instance();
+//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//        return parent -> new ModsScreen(parent);
 //    }
 //}
