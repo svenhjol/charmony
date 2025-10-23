65153d2fb021593e17910dae1955bbca40b0ee30
Sven H <sven.hjol@protonmail.com>
2025-10-23 01:09:38 +0000

    Remove unused task query

--- mods/villager_tasks/src/main/java/charmony/villager_tasks/common/features/villager_tasks/enums/TaskQuery.java
+++ mods/villager_tasks/src/main/java/charmony/villager_tasks/common/features/villager_tasks/enums/TaskQuery.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 
 public enum TaskQuery implements StringRepresentable {
     Unspecified("unspecified"),
-    Update("update"),
     Abandon("abandon"),
     Accept("accept"),
     Complete("complete");
