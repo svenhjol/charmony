fb120e533b4ce5fe449f1ebbe7c632aca344161a
Sven H <sven.hjol@protonmail.com>
2025-12-30 16:38:47 +0000

    Move common code for lookingAtBlock into helper

--- mods/core/src/main/java/charmony/core/helpers/PlayerHelper.java
+++ mods/core/src/main/java/charmony/core/helpers/PlayerHelper.java
@@ -2,6 +2,7 @@ package charmony.core.helpers;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 
@@ -11,5 +12,13 @@ import java.util.List;
 public final class PlayerHelper {
     public static List<Player> getPlayersInRange(Level level, BlockPos pos, double range) {
         return level.getEntitiesOfClass(Player.class, new AABB(pos).inflate(range));
+    }
+
+    public static BlockPos lookingAtBlock(Player player) {
+        var cameraPosVec = player.getEyePosition(1.0f);
+        var rotationVec = player.getViewVector(1.0f);
+        var vec3d = cameraPosVec.add(rotationVec.x * 6, rotationVec.y * 6, rotationVec.z * 6);
+        var raycast = player.level().clip(new ClipContext(cameraPosVec, vec3d, ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, player));
+        return raycast.getBlockPos();
     }
 }
